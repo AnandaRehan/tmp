03263528f5882724aef42d2b80391880bf91c407
AnandaRehan <anandarehan071@gmail.com>
2026-08-27 20:37:51 +0700

    tmp 1

---
+++ app/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+
+android {
+    namespace = "com.ehan.app1"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.ehan.app1"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    
+    buildFeatures {
+        compose = true
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+
+    // Jetpack Compose
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    implementation(libs.androidx.datastore.preferences)
+
+    // Retrofit & OkHttp
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.okhttp.logging)
+}
---
+++ app/src/main/java/com/ehan/app1/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.ehan.app1
+
+import android.os.Bundle
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.getValue
+import com.ehan.app1.ui.theme.App1Theme
+import com.ehan.app1.ui.theme.ThemeMode
+
+class MainActivity : ComponentActivity() {
+    private val Context.dataStore by preferencesDataStore(name = "user_preferences")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            App1Theme {
+                greeting(dataStore)
+            }
+        }
+    }
+}
+
+@Composable
+fun greeting(private val dataStore: DataStore<Preferences>) {
+    private val DATA1_KEY = stringPreferencesKey("data1")
+    val _angka_1: String = dataStore.data.map { preferences ->
+            preferences[DATA1_KEY] ?: "0"
+    }
+    val angka_1: Int = when (_angka_1.toIntOrNull()) {
+        "null" -> 0
+        else -> _angka_1.toInt() ?: 0
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .systemBarsPadding()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Text(text = "Angka Saat Ini " + angka_1.toString())
+        Button(
+            onClick = {
+                angka_1++
+                dataStore.edit { preferences ->
+                    preferences[DATA1_KEY] = angka_1.toString()
+                }
+            }
+        ) {
+            Text(
+                text = "Tambah 1"
+            )
+        }
+    }
+}
